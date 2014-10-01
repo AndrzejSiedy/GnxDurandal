adf945a12901948ef966764ac7a2b872a13cdac0
wilk666 <andre@cartomatic.pl>
2014-10-01 14:52:11 +0200

    Add seeding - doesn't bloody work

--- Gnx/Gnx/Migrations/Configuration.cs
+++ Gnx/Gnx/Migrations/Configuration.cs
@@ -1,9 +1,13 @@
 namespace Gnx.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+    using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Helpers;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Gnx.Models.ApplicationDbContext>
     {
@@ -12,8 +16,65 @@ namespace Gnx.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+        private UserManager<IdentityUser> _UserManager { get; set; }
+
+        private RoleManager<IdentityRole> _RoleManager { get; set; }
+
         protected override void Seed(Gnx.Models.ApplicationDbContext context)
         {
+
+            _UserManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(context));
+            _RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            try
+            {
+                // create set of roles
+                var adminRole = new IdentityRole()
+                {
+                    Name = "administrator"
+                };
+                var createAdminRole = _RoleManager.CreateAsync(adminRole);
+
+                var superUserRole = new IdentityRole()
+                {
+                    Name = "superuser"
+                };
+                var createSuperUserRole = _RoleManager.CreateAsync(superUserRole);
+
+                var authenticatedUserRole = new IdentityRole()
+                {
+                    Name = "authenticated"
+                };
+                var createAuthenticatedUserRole = _RoleManager.CreateAsync(authenticatedUserRole);
+                var guestUserRole = new IdentityRole()
+                {
+                    Name = "guest"
+                };
+                var createGuestUserRole = _RoleManager.CreateAsync(guestUserRole);
+
+
+
+
+                // create admin user
+                var admin = new IdentityUser
+                {
+                    UserName = "admin",
+                    Email = "admin@geonetix.pl",
+                    PasswordHash = Crypto.HashPassword("test111")
+                };
+
+                IdentityResult resultCreateAdmin = _UserManager.Create(admin, "test111");
+
+
+                var userAdmin = _UserManager.FindByEmail("admin@geonetix.pl");
+                var roleresult = _UserManager.AddToRole(userAdmin.Id, "administrator");
+            }
+            catch (Exception ex)
+            {
+                string stoper = ex.Message;
+            }
+
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
